fbd0d21d04581c5bf35c60cd9d251c8744ab1f0c
pineapple789 <pavel_turaev2828@mail.ru>
2022-10-12 23:19:43 +0800

    new file

---
+++ defLibsDefs.h
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <assert.h>
+
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <functional>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double db;
+
+const long long INF = 1e18;
+const long long MD1 = 7 + 1e9;
+const long long MXLN = 1 << 17;
+const double PI = atan2(0, -1);
+
